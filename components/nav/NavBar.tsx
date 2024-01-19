4888c543ad3a31ed052853c8ad34d8b6730f0f9a
qorlgns1 <rlgns0610@gmail.com>
2024-01-19 17:21:39 +0900

    feat: navbar side blur effect and change structure

--- components/nav/NavBar.tsx
+++ components/nav/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 import styled from '@emotion/styled'
 
@@ -7,21 +7,72 @@ import NavLink from '@/components/nav/NavLink'
 import type { Nav } from '@/constants/nav'
 
 interface Props {
-  nav: Nav[][]
+  nav: Nav[]
 }
 
 const NavBar = ({ nav }: Props) => {
+  const navRef = useRef<HTMLDivElement>(null)
+  const ulRef = useRef<HTMLUListElement>(null)
+
+  useEffect(() => {
+    const $nav = navRef.current
+    const $ul = ulRef.current
+
+    if (!$nav || !$ul) return
+
+    const navSidePadding = parseInt(
+      window.getComputedStyle($nav).padding.split(' ')[1].replace('px', ''),
+      10
+    )
+
+    // overflow 된 것까지 포함한 너비
+    const navWidth = $ul.scrollWidth + navSidePadding * 2
+    const bodyWidth = document.body.clientWidth
+
+    if (navWidth > bodyWidth) {
+      $ul.classList.add('right-blur')
+    }
+
+    const addBlurToNavSide = () => {
+      const firstLi = $ul.children.item(0)
+      const lastLi = $ul.children.item($ul.children.length - 1)
+
+      const firstLiRect = firstLi?.getBoundingClientRect()
+      const lastLiRect = lastLi?.getBoundingClientRect()
+
+      if (!firstLiRect || !lastLiRect) return
+
+      if (firstLiRect.left < navSidePadding / 2) {
+        $ul.classList.add('left-blur')
+      } else {
+        $ul.classList.remove('left-blur')
+      }
+
+      if (lastLiRect.right > bodyWidth - navSidePadding / 2) {
+        $ul.classList.add('right-blur')
+      } else {
+        $ul.classList.remove('right-blur')
+      }
+    }
+
+    $ul.addEventListener('scroll', addBlurToNavSide)
+    window.addEventListener('resize', addBlurToNavSide)
+
+    return () => {
+      $ul.removeEventListener('scroll', addBlurToNavSide)
+      window.removeEventListener('resize', addBlurToNavSide)
+    }
+  }, [])
+
   return (
-    <Navigation>
-      {nav.map((nav, index) => (
-        <Ul key={index}>
-          {nav.map((item, index) => (
-            <li key={index}>
-              <NavLink href={item.link}>{item.title}</NavLink>
-            </li>
-          ))}
-        </Ul>
-      ))}
+    <Navigation ref={navRef}>
+      <Ul ref={ulRef}>
+        {nav.map((item, index) => (
+          <li key={index}>
+            <NavLink href={item.link}>{item.title}</NavLink>
+          </li>
+        ))}
+      </Ul>
     </Navigation>
   )
 }
@@ -31,26 +82,26 @@ const Navigation = styled.nav`
   display: flex;
   align-items: center;
   border-bottom: 1px solid #ddd;
+  position: relative;
 
-  > ul:first-of-type {
-    ::after {
-      content: '';
-      display: block;
-      height: 3.2rem;
-      margin: 0 0.8rem;
-      width: 1px;
+  @media (min-width: 768px) {
+    > ul > li:nth-child(3) {
+      position: relative;
+      display: flex;
+      align-items: center;
 
-      border-right: 1px solid #ddd;
+      :after {
+        content: '';
+        display: block;
+        position: absolute;
+        right: -1.5rem;
 
-      background-color: #ddd;
-    }
-  }
+        width: 1px;
+        height: 3.2rem;
 
-  > ul:last-of-type {
-    overflow-x: auto;
-
-    &::-webkit-scrollbar {
-      display: none;
+        border-right: 1px solid #ddd;
+        background-color: #ddd;
+      }
     }
   }
 `
@@ -59,7 +110,55 @@ const Ul = styled.ul`
   display: flex;
   align-items: center;
   gap: 3rem;
-  padding-left: 2rem;
+
+  overflow-x: auto;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  &.left-blur {
+    ::before {
+      content: '';
+      width: 150px;
+      height: 52px;
+      position: absolute;
+      background: linear-gradient(270deg, #fff0 0, #fff 90%, white);
+      left: 0;
+    }
+  }
+
+  &.right-blur {
+    ::after {
+      content: '';
+      width: 150px;
+      height: 52px;
+      position: absolute;
+      background: linear-gradient(90deg, #fff0 0, #fff 90%, white);
+      right: 0;
+    }
+  }
+
+  @media (min-width: 768px) {
+    & > li:nth-child(3) {
+      position: relative;
+      display: flex;
+      align-items: center;
+
+      :after {
+        content: '';
+        display: block;
+        position: absolute;
+        right: -1.5rem;
+
+        width: 1px;
+        height: 3.2rem;
+
+        border-right: 1px solid #ddd;
+        background-color: #ddd;
+      }
+    }
+  }
 `
 
 export default NavBar
--- constants/nav.ts
+++ constants/nav.ts
@@ -3,59 +3,55 @@ export interface Nav {
   link: string
 }
 
-export const NAV_LIST: Nav[][] = [
-  [
-    {
-      title: '보도/편집 전용',
-      link: '/',
-    },
-    {
-      title: '팔로잉',
-      link: '/following',
-    },
-    {
-      title: 'Will Photo+',
-      link: '/will-photo-plus',
-    },
-  ],
-  [
-    {
-      title: '단색',
-      link: '/colors',
-    },
-    {
-      title: '배경 화면',
-      link: '/wallpapers',
-    },
-    {
-      title: '3D 렌더링',
-      link: '/3d-rendering',
-    },
-    {
-      title: '자연',
-      link: '/nature',
-    },
-    {
-      title: '텍스처 및 패턴',
-      link: '/textures-and-patterns',
-    },
-    {
-      title: '건축 및 인테리어',
-      link: '/architecture-and-interiors',
-    },
-    {
-      title: '필름',
-      link: '/film',
-    },
-    {
-      title: '거리 사진',
-      link: '/street-photography',
-    },
-    {
-      title: '실험적인',
-      link: '/experimental',
-    },
-  ],
+export const NAV_LIST: Nav[] = [
+  {
+    title: '보도/편집 전용',
+    link: '/',
+  },
+  {
+    title: '팔로잉',
+    link: '/following',
+  },
+  {
+    title: 'Will Photo+',
+    link: '/will-photo-plus',
+  },
+  {
+    title: '단색',
+    link: '/colors',
+  },
+  {
+    title: '배경 화면',
+    link: '/wallpapers',
+  },
+  {
+    title: '3D 렌더링',
+    link: '/3d-rendering',
+  },
+  {
+    title: '자연',
+    link: '/nature',
+  },
+  {
+    title: '텍스처 및 패턴',
+    link: '/textures-and-patterns',
+  },
+  {
+    title: '건축 및 인테리어',
+    link: '/architecture-and-interiors',
+  },
+  {
+    title: '필름',
+    link: '/film',
+  },
+  {
+    title: '거리 사진',
+    link: '/street-photography',
+  },
+  {
+    title: '실험적인',
+    link: '/experimental',
+  },
 ]
 
 export const BOOKMARK_NAV_LIST: Nav[] = [
